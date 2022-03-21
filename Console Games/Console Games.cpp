57e4b681e574ba762fa327c008b677fd2bb6fe4f
evana <evana2@illinois.edu>
2022-03-20 21:28:40 -0500

    game selection

--- Console Games/Console Games.cpp
+++ Console Games/Console Games.cpp
@@ -6,17 +6,41 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    //CN4::runConnectFour();
-    TTT::runTTT();
+
+    while (true) {
+        std::cout << "Select a game to play:" << endl;
+        std::cout << "(1) Connect Four" << endl;
+        std::cout << "(2) Tic Tac Toe" << endl;
+        std::cout << "(3) Quit" << endl;
+        std::cout << "Selection: ";
+        int userChoice;
+        cin >> userChoice;
+
+        while (1)
+        {
+            if (cin.fail() || userChoice < 1 || userChoice > 3)
+            {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout << "Invalid input, please try again: ";
+                cin >> userChoice;
+            }
+            else {
+                break;
+            }
+        }
+
+        switch (userChoice)
+        {
+        case 1:
+            CN4::runConnectFour();
+            break;
+        case 2:
+            TTT::runTTT();
+            break;
+        case 3:
+            return 0;
+        }
+
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
